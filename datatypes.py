504001ca10eea82e1648d1402e1b8f78d09d1362
YUSRA SHAHID <99241818+yusrashahid205@users.noreply.github.com>
2022-09-17 16:14:33 -0700

    Add files via upload

---
+++ datatypes.py
@@ -0,0 +1,18 @@
+a="yusra"
+b=35
+c=21.9
+d=True
+
+#print the values
+
+print(a)
+print(b)
+print(c)
+print(d)
+
+#printing the type of variables
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
